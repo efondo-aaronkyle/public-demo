c67914c7cf120afb7700723936370446709da84b
efondo-aaronkyle <akyleefondo@gmail.com>
2025-07-04 17:11:04 +0800

    add route

--- app.py
+++ app.py
@@ -6,5 +6,9 @@ app = Flask(__name__)
 def homepage():
     return "Welcome to iskolutions"
 
+@app.route("/<name>")
+def display_name(name):
+    return f"Welcome {name}"
+
 if __name__ == "__main__":
     app.run()
